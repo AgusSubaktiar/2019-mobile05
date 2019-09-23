6c948efbf448473e3a47263547376a9e1b20313b
Agus Subag <456jojon>
2019-09-23 14:48:30 +0700

    package Fragments blue and red

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragments/RedFragmen.java
@@ -0,0 +1,16 @@
+package polinema.ac.id.starterchapter05.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class RedFragmen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_red_fragmen);
+    }
+}
