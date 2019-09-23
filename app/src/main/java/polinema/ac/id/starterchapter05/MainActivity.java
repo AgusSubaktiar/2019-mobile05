50d0bdf73bef6d82978519dec7c85cb09dc52ba4
Agus Subag <456jojon>
2019-09-23 14:34:09 +0700

    Edit MainActive

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,11 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.activities.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void handlerStaticFragmen(View view) {
+    }
+
+    public void handlerDynamicFragmen(View view) {
+    }
+
+    public void handlerPratikumFragmen(View view) {
+        Intent i = new Intent( this, StaticActivity.class);
+        startActivity(i);
+    }
 }
+
